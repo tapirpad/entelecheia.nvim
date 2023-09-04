2a415011545876879ddd1fde046e08d53a5ab267
Pradipta <ignasius.pradipta@hotmail.com>
2023-09-04 13:13:46 -0500

    Adjust for Iosevka Light

--- lua/lualine/themes/entelecheia.lua
+++ lua/lualine/themes/entelecheia.lua
@@ -27,19 +27,26 @@ local colors = {
 -- LuaFormatter on
 return {
   normal = {
-    a = {fg = colors.gray4, bg = colors.green, gui = 'bold'},
+    -- a = {fg = colors.gray4, bg = colors.green, gui = 'bold'},
+    a = {fg = colors.black, bg = colors.green, gui = 'bold'},
     b = {fg = colors.fg, bg = colors.gray2},
     c = {fg = colors.fg, bg = colors.bg},
     x = {fg = colors.fg, bg = colors.bg},
     y = {fg = colors.fg, bg = colors.gray2},
-    z = {fg = colors.gray4, bg = colors.green, gui = 'bold'},
+    -- z = {fg = colors.gray4, bg = colors.green, gui = 'bold'},
+    z = {fg = colors.black, bg = colors.green, gui = 'bold'},
   },
-  insert = {a = {fg = colors.bg, bg = colors.blue, gui = 'bold'}},
-  visual = {a = {fg = colors.bg, bg = colors.purple, gui = 'bold'}},
-  command = {a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'}},
-  replace = {a = {fg = colors.bg, bg = colors.red1, gui = 'bold'}},
+  -- insert  = {a = {fg = colors.bg, bg = colors.blue, gui = 'bold'}},
+  -- visual  = {a = {fg = colors.bg, bg = colors.purple, gui = 'bold'}},
+  -- command = {a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'}},
+  -- replace = {a = {fg = colors.bg, bg = colors.red1, gui = 'bold'}},
+  insert  = {a = {fg = colors.black, bg = colors.blue, gui = 'bold'}},
+  visual  = {a = {fg = colors.black, bg = colors.purple, gui = 'bold'}},
+  command = {a = {fg = colors.black, bg = colors.yellow, gui = 'bold'}},
+  replace = {a = {fg = colors.black, bg = colors.red1, gui = 'bold'}},
   inactive = {
-    a = {fg = colors.gray1, bg = colors.bg, gui = 'bold'},
+    -- a = {fg = colors.gray1, bg = colors.bg, gui = 'bold'},
+    a = {fg = colors.black, bg = colors.bg, gui = 'bold'},
     b = {fg = colors.gray1, bg = colors.bg},
     c = {fg = colors.gray1, bg = colors.bg}
   }
